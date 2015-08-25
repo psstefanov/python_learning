ca99b058061c558aa49fa92265141bbdfd5aeb18
Pavkata Stefanov <pstefanov111@axway.com>
2015-08-25 16:48:28 +0300

    Add files

--- 20.py
+++ 20.py
@@ -1,8 +1,12 @@
-def translate(english):
-    swedish = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+def translate(english2):
+    dictionary = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+    english = english2.split()
+    translate = []
     for i in (english):
-        if  (i.upper()  in (char.upper() for char in  swedish)):
-            print (swedish[i])
+        if  (i.upper()  in (char.upper() for char in  dictionary)):
+                translate.append(dictionary[i])
+    swedish = ' '.join(translate)
+    print (swedish)
 
-english = raw_input("Enter English word: ")
-translate(english)
+english2 = raw_input("Enter English word: ")
+translate(english2)
