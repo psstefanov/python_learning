bfc950ae630ead95fe3a154c2bd50a97ac1f7130
Pavkata Stefanov <pstefanov111@axway.com>
2015-08-25 16:32:39 +0300

    Add more projects

--- 20.py
+++ 20.py
@@ -1,4 +1,8 @@
 def translate(english):
-    swedish = {"merry":"god", "christmas":"jul", "and":"och", "happy":gott", "new":"nytt", "year":"år"}
-    if  (english.upper()  in (char.upper() for char in  swedish)):
-        print "dict['Alice']"
+    swedish = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+    for i in (english):
+        if  (i.upper()  in (char.upper() for char in  swedish)):
+            print (swedish[i])
+
+english = raw_input("Enter English word: ")
+translate(english)
